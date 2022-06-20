5ba137cbcea0cd716b2618ba4e531d9d0159a27c
Erik Giertl <erik.giertl@icloud.com>
2022-06-21 01:34:42 +0200

    Added ForecastListViewModel. Minor changes and making code more clear.

---
+++ WeatherApp/ForecastListViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ForecastListViewModel.swift
+//  WeatherApp
+//
+//  Created by Erik Kokinda on 21/06/2022.
+//
+
+import CoreLocation
+import Foundation
+
+class ForecastListViewModel: ObservableObject {
+    @Published var forecasts: [ForecastViewModel] = []
+    var location: String = ""
+    
+    func getWeatherForecast() {
+        let apiService = APIService.shared
+        
+        CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            if let lat = placemarks?.first?.location?.coordinate.latitude,
+               let lon = placemarks?.first?.location?.coordinate.longitude
+            {
+            apiService.getJSON(urlString: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,hourly&units=metric&appid=678dd1f7ce03ba9f6ed9804d363bb4ef", dateDecodingStrategy: .secondsSince1970)
+            { (result: Result<Forecast,APIService.APIError>) in
+                switch result {
+                case .success(let forecast):
+                    DispatchQueue.main.async {
+                    self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0)}
+                    }
+                case .failure(let apiError):
+                    switch apiError {
+                    case .error(let errorString):
+                        print(errorString)
+                    }
+                }
+            }
+            }
+        }
+
+    }
+}
