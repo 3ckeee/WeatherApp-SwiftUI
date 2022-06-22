02eefc29f4b5ff33f09888c5967345792a47ef53
Erik Giertl <erik.giertl@icloud.com>
2022-06-22 23:02:48 +0200

    UI enhancment

--- WeatherApp/ForecastListViewModel.swift
+++ WeatherApp/ForecastListViewModel.swift
@@ -10,9 +10,16 @@ import Foundation
 import SwiftUI
 
 class ForecastListViewModel: ObservableObject {
+    struct AppError: Identifiable {
+        let id = UUID().uuidString
+        let errorString: String
+    }
+    
     @Published var forecasts: [ForecastViewModel] = []
+    var appError: AppError? = nil
+    @Published var isLoading: Bool = false
     @AppStorage("location") var location: String = ""
-    @AppStorage("system")var system: Int = 0 {
+    @AppStorage("system") var system: Int = 0 {
         didSet {
             for i in 0..<forecasts.count {
                 forecasts[i].system = system
@@ -24,36 +31,40 @@ class ForecastListViewModel: ObservableObject {
         if location != "" {
             getWeatherForecast()
         }
-        
     }
     
-    
     func getWeatherForecast() {
+        isLoading = true
         let apiService = APIService.shared
-        
         CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
             if let error = error {
+                self.isLoading = false
+                self.appError = AppError(errorString: error.localizedDescription)
                 print(error.localizedDescription)
             }
             if let lat = placemarks?.first?.location?.coordinate.latitude,
-               let lon = placemarks?.first?.location?.coordinate.longitude
-            {
-            apiService.getJSON(urlString: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,hourly&units=metric&appid=678dd1f7ce03ba9f6ed9804d363bb4ef", dateDecodingStrategy: .secondsSince1970)
-            { (result: Result<Forecast,APIService.APIError>) in
-                switch result {
-                case .success(let forecast):
-                    DispatchQueue.main.async {
-                        self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0, system: self.system)}
-                    }
-                case .failure(let apiError):
-                    switch apiError {
-                    case .error(let errorString):
-                        print(errorString)
+               let lon = placemarks?.first?.location?.coordinate.longitude {
+                // Don't forget to use your own key
+                        apiService.getJSON(urlString: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,hourly&units=metric&appid=678dd1f7ce03ba9f6ed9804d363bb4ef",
+                                           dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast,APIService.APIError>) in
+                    switch result {
+                    case .success(let forecast):
+                        DispatchQueue.main.async {
+                            self.isLoading = false
+                            self.forecasts = forecast.daily.map { ForecastViewModel(forecast: $0, system: self.system)}
+                        }
+                    case .failure(let apiError):
+                        switch apiError {
+                        case .error(let errorString):
+                            self.isLoading = false
+                            self.appError = AppError(errorString: errorString)
+                            print(errorString)
+                        }
                     }
                 }
-            }
             }
         }
 
     }
+    
 }
